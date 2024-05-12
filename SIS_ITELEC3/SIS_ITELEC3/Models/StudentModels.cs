622dd9869acd5fe0c47d08596b8ccf567758c361
arias-clarq <clarqarias@gmail.com>
2024-05-12 22:46:24 +0800

    initial-migration test

---
+++ SIS_ITELEC3/SIS_ITELEC3/Models/StudentModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIS_ITELEC3.Models
+{
+    public class StudentModels
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime Year { get; set; }
+        public string Section { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
